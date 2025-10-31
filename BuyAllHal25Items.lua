62e8f54a6654e02697c7d66389220aa4a4f09cfa
dxbjamie <38177736+dxbjamie@users.noreply.github.com>
2025-10-31 17:15:27 +0700

    Update BuyAllHal25Items.lua

--- BuyAllHal25Items.lua
+++ BuyAllHal25Items.lua
@@ -1,6 +1,6 @@
 --Buy Hal 2025 Items
-repeat wait(0.01) until game.Players.LocalPlayer ~= nil
-if not game.Loaded then  game.Loaded:Wait() end --wait for game to fully load
+--repeat wait(0.01) until game.Players.LocalPlayer ~= nil
+--if not game.Loaded then  game.Loaded:Wait() end --wait for game to fully load
 --wait(180)
 ---loadstring(game:HttpGet("https://pastebin.com/raw/Cn25rnPi"))()
 --loadstring(game:HttpGet"https://drive.google.com/uc?id=1cOwY1kpj1E819fID-FTpyMBCuRsURNZj"))()
@@ -83,4 +83,5 @@ wait(1)
 game:GetService("ReplicatedStorage").API["ShopAPI/BuyItem"]:InvokeServer("gifts", "halloween_2025_sticker_pack",{})
 wait(1)
 game:GetService("ReplicatedStorage").API["ShopAPI/BuyItem"]:InvokeServer("pets", "halloween_2025_slimingo",{})
+
 
